423743335efcd1e32c88a4f07f0749bce0159dd5
chermehdi <mehdi.cheracher@gmail.com>
2019-07-27 12:11:05 +0100

    add domain models tests

---
+++ src/test/java/io/github/chermehdi/mts/domain/AccountTest.java
@@ -0,0 +1,31 @@
+package io.github.chermehdi.mts.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import io.github.chermehdi.mts.util.validation.ValidationException;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author chermehdi
+ */
+class AccountTest {
+
+  @Test
+  void testCanCreateAnAccount() {
+    var account = new Account();
+  }
+
+  @Test
+  void testCannotCreateAccountWithNegativeBalance() {
+    var ex = assertThrows(ValidationException.class,
+        () -> new Account("", BigDecimal.valueOf(-1.0)));
+    assertEquals("Cannot create an account with negative balance", ex.getMessage());
+  }
+
+  @Test
+  void testCannotCreateAccountWithNullBalance() {
+    assertThrows(ValidationException.class, () -> new Account("", null));
+  }
+}
